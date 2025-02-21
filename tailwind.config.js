af004ea1a1744e5bb3b9f63b35642d0f26ca3382
Nerdyxils <silasabiodun25@gmail.com>
2025-02-21 18:58:03 -0500

    Fix Tailwind PostCSS issue for Netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust paths to match your project
+  theme: {
+    extend: {}, // Add your custom styles here
+  },
+  plugins: [], // Add any Tailwind plugins if needed
+};
